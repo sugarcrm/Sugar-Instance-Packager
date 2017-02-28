83a65cfd64913c415a2a17a06bf084f136c98362
jclark <geraldbclark@gmail.com>
2017-02-27 23:23:35 -0500

    tests

---
+++ tests/MySQLTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Sugarcrm\Support\Helpers\Packager\Tests\;
+
+use Sugarcrm\Support\Helpers\Internal\API\Clients;
+use Sugarcrm\Support\Helpers\Internal\API\Scarlett\Client as Scarlett;
+
+/**
+ * Class ClientsTest
+ * @package Sugarcrm\Support\Helpers\Internal\Tests\API
+ * @group support
+ */
+class ClientsTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Initialize objects
+     */
+    public static function setUpBeforeClass()
+    {
+    }
+
+    /**
+     * Remove objects
+     */
+    public static function tearDownAfterClass()
+    {
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * @covers ::getProperties
+     * @covers ::isClient
+     * @group clients
+     */
+    public function testBackup(){
+        $time = time();
+
+        $namespace = '\\Sugarcrm\\Support\\Helpers\\Packager\\Instance\\MySQL\\Packager';
+
+        $packager = new $namespace(
+            'sugar',
+            'backups',
+            $time
+        );
+
+        $this->assertTrue(file_exists("backups/{$time}-files.zip"));
+        $this->assertTrue(file_exists("backups/{$time}-db.zip"));
+        $packager->pack();
+    }
+}
