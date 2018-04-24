9bbcea40f9c57221b6a811efe33f7618931bd184
jwigg <jwigglesworth@sugarcrm.com>
2018-04-24 16:46:20 -0400

    start work on making uploads possible/optional

--- execute.php
+++ execute.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 
 $longopts = array("source:", "destination:", "name:", "type::");
@@ -15,12 +14,26 @@ define('APPNAME', \Sugarcrm\Support\Helpers\Packager\Instance\Abstracted\Package
 define('VERSION', \Sugarcrm\Support\Helpers\Packager\Instance\Abstracted\Packager::VERSION );
 
 
-$namespace = '\\Sugarcrm\\Support\\Helpers\\Packager\\Instance\\' . $options['type'] . '\\Packager';
 
-$packager = new $namespace(
-    $options['source'],
-    $options['destination'],
-    $options['name']
 );
 
-$packager->pack();
+//allow uploading to be completed separate from packing
+//if upload isn't empty, then that's the package we're trying to upload
+//otherwise, create a new package
+if (!empty($options['upload'])) {
+    $package = $options['upload'];
+    if (!is_file($package)) {
+        print "Could not read package ${package}; make sure it exists and its permissions allow reading\n";
+    }
+} else {
+    $namespace = '\\Sugarcrm\\Support\\Helpers\\Packager\\Instance\\' . $options['type'] . '\\Packager';
+
+    $packager = new $namespace(
+        $options['source'],
+        $options['destination'],
+        $options['name']
+    );
+
+    $packager->pack();
+}
+
